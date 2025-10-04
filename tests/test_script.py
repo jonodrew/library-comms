9a2a0f07115b1a161e3e8b5f388901743a079b38
mariaroot <mariaroot1@gmail.com>
2025-10-04 11:45:43 +0100

    Add test and function for reading from csv

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,2 +1,6 @@
-def test_code():
-    assert True is False
+from src.read import get_details
+
+
+def test_get_details():
+    details = get_details(filepath="tests/test_data.csv")
+    assert {"Student_name": "Harry", "Student_ID": "ABC123", "Student_phone_number": "07777777777"} in details
