78541127eb7590b04fdbc7e83d0ae65c54ba4ed3
jonathan.kerr <3410350+jonodrew@users.noreply.github.com>
2025-11-04 14:33:27 +0000

    Update the handler to process the event
    This code deals with the notification from S3, processing it in line with what we expect to happen. The logic of what to do with the CSV file is kept separate from the handler, which just opens and reads the CSV.
    
    At the moment we just print the message, rather than sending it for real

--- src/index.py
+++ src/index.py
@@ -1,3 +1,27 @@
+from typing import Iterator
+
+import boto3
+import csv
+from message import create_message
+
+
 def handler(event, context):
-    print(f"Received event: {event}")
-    return "Hello from Lambda triggered by S3!"
+    s3 = boto3.resource('s3')
+
+    # get the details of the thing that was just created in S3
+    detail = event['Records'][0]['s3']
+    bucket_name: str = detail['bucket']['name']
+    object_name: str = detail['object']['key']
+
+    # get the actual thing that was just created
+    csv_file = s3.Object(bucket_name=bucket_name, key=object_name)
+    response = csv_file.get()
+    data = response['Body'].read()
+
+    # read the data into dictionaries using csv library
+    rows: Iterator[dict[str, str]] = csv.DictReader(data.decode().splitlines())
+
+    # iterate over each row and process it
+    for row in rows:
+        print(create_message(row))
+        # process_row(row)
